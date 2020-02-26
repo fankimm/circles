7c9fd6a09cb1822f09eb7000ea3ae6bce818b00f
fankimm <crakim7@naver.com>
2020-02-26 18:12:57 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,30 @@
-function setup(){
-createCanvas(windowWidth,windowHeight)
-background(0,0,0)
+
+function setup() {
+  // put setup code here
+  let density = displayDensity();
+  pixelDensity(density);
+  createCanvas (displayWidth, displayHeight)
+  noStroke()
+
 }
 
 function draw(){
+  if(displayWidth<displayHeight){
+    background(255,0,0)
+  } else {
+    background(0,255,0)
+  }
 
-if (windowWidth<windowHeight){
-backgroud(255,0,0)
-} else {
-background(0,255,0)
+  fill(0)
+  text("W : " , 10,10)
+  text("H : " , 10,20)
+  text(windowWidth, 30, 10)
+  text(windowHeight, 30, 20)
+
 }
 
-text(“W : “, 10,10)
-text(“H : “, 10,20)
-text(windowWidth,20,10)
-text(windowHeight,20,20)
 
+function touchMoved(){
+
+  return false
 }
