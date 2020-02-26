37a35fd1c5a3aba8aa492b8ca8746080f31ed3d7
fankimm <crakim7@naver.com>
2020-02-26 15:57:22 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var r = 30
+
+function setup() {
+  // put setup code here
+  let density = displayDensity();
+  pixelDensity(density);
+  createCanvas (windowWidth, windowHeight)
+  background(255)
+  noStroke()
+
+  mkCircle()
+}
+
+function mkCircle(){
+  for (var i = 0; i<100; i++){
+    for (var j = 0; j<100; j++){
+      fill(random(0,255),random(0,255),random(0,255))
+      circle(i*r, j*r, r)
+    }
+  }
+}
+
+function mouseClicked(){
+  mkCircle()
+}
