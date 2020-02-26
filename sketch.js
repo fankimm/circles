b8a46bf1da074cb791fa1a0467bd0c5f38418c38
fankimm <crakim7@naver.com>
2020-02-26 17:59:28 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 function setup(){
-createCanvas(displayWidth,displayHeight)
+createCanvas(windowWidth,windowHeight)
 background(0,0,0)
 }
 
 function draw(){
 
-if (displayWidth<displayHeight){
+if (windowWidth<windowHeight){
 backgroud(255,0,0)
 } else {
 background(0,255,0)
@@ -13,7 +13,7 @@ background(0,255,0)
 
 text(“W : “, 10,10)
 text(“H : “, 10,20)
-text(displayWidth,20,10)
-text(displayHeight,20,20)
+text(windowWidth,20,10)
+text(windowHeight,20,20)
 
 }
