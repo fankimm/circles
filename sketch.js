79f5d330cb614a3d85c527d7198cffff4454ca38
fankimm <crakim7@naver.com>
2020-02-27 09:38:27 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,8 +28,8 @@ text("X : " , 10,30)
 text("Y : ", 10,40)
   text(windowWidth, 30, 10)
   text(windowHeight, 30, 20)
-text(touches[0], 30,30)
-text(touches[1], 30,40)
+text(touches[0].x, 30,30)
+text(touches[0].y, 30,40)
 
 }
 function windowResized(){
