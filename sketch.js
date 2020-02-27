830c77f805724299eecbb16061a0d0c268c0bb4a
fankimm <crakim7@naver.com>
2020-02-27 10:29:04 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,11 @@
+var isMobile
 
 function setup() {
 // put setup code here
+  if(displayWidth>displayHeight){
+    isMobile = false
+  }
+
 let density = displayDensity();
 pixelDensity(density);
 
@@ -30,17 +35,28 @@ resizeCanvas(windowWidth,windowHeight)
 }
 function touchStarted(){
 
-myConsole()
-drawCircle()
+if(isMobile){
+    myConsole()
+  drawCircle()
+
+  }
+
 }
 function touchMoved(){
-myConsole()
-drawCircle()
+if(isMobile){
+    myConsole()
+  drawCircle()
+
+  }
+
 return false
 }
 function touchEnded(){
-myConsole()
-drawCircle()
+  if(isMobile){
+    myConsole()
+  drawCircle()
+
+  }
 
 
 }
@@ -61,7 +77,47 @@ text(touches[0].y, 30,40)
 
 }
 
+function myConsoleM(){
+    fill(255)
+  rect(0,0,50,50)
+fill(0)
+text("W : " , 10,10)
+text("H : " , 10,20)
+text("X : " , 10,30)
+text("Y : ", 10,40)
+text(windowWidth, 30, 10)
+text(windowHeight, 30, 20)
+text(mouseX, 30,30)
+text(mouseY, 30,40)
+
+}
+
 function drawCircle(){
   fill(random(0,255),random(0,255),random(0,255))
   circle(touches[0].x,touches[0].y,30)
+}
+function drawCircleM(){
+  fill(random(0,255),random(0,255),random(0,255))
+  circle(mouseX,mouseY,30)
+}
+
+function mousePressed(){
+  if(!isMobile){
+    myConsoleM()
+    drawCircleM()
+  }
+}
+function mouseDragged(){
+    if(!isMobile){
+    myConsoleM()
+    drawCircleM()
+  }
+
+}
+function mouseReleased(){
+    if(!isMobile){
+    myConsoleM()
+    drawCircleM()
+  }
+
 }
