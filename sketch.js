26223551738da622559d28a0604223d6040dba03
fankimm <crakim7@naver.com>
2020-02-27 09:39:33 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,8 +28,6 @@ text("X : " , 10,30)
 text("Y : ", 10,40)
   text(windowWidth, 30, 10)
   text(windowHeight, 30, 20)
-text(touches[0].x, 30,30)
-text(touches[0].y, 30,40)
 
 }
 function windowResized(){
@@ -37,6 +35,9 @@ resizeCanvas(windowWidth,windowHeight)
 }
 
 function touchMoved(){
+
+text(touches[0].x, 30,30)
+text(touches[0].y, 30,40)
 
   return false
 }
