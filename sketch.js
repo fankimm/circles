aa2b0a4d304a2be7ca58e27a121efb79401fce04
fankimm <crakim7@naver.com>
2020-02-27 10:15:16 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 
 function setup() {
-  // put setup code here
-  let density = displayDensity();
-  pixelDensity(density);
- 
-  noStroke()
+// put setup code here
+let density = displayDensity();
+pixelDensity(density);
+
+noStroke()
 
 createCanvas(windowWidth, windowHeight)
 
@@ -12,22 +12,15 @@ createCanvas(windowWidth, windowHeight)
 
 function draw(){
 
-  
-  if(windowWidth<windowHeight){
 
-    //background(255,0,0)
-  } else {
+if(windowWidth<windowHeight){
+
+//background(255,0,0)
+} else {
 resizeCanvas(windowWidth,windowHeight)
-    //background(0,255,0)
-  }
+//background(0,255,0)
+}
 
-  fill(0)
-  text("W : " , 10,10)
-  text("H : " , 10,20)
-text("X : " , 10,30)
-text("Y : ", 10,40)
-  text(windowWidth, 30, 10)
-  text(windowHeight, 30, 20)
 
 
 
@@ -36,24 +29,29 @@ function windowResized(){
 resizeCanvas(windowWidth,windowHeight)
 }
 function touchStarted(){
-background(255)
-text(touches[0].x, 30,30)
-text(touches[0].y, 30,40)
-
+myConsole()
 }
 function touchMoved(){
+myConsole()
 
-background(255)
-
-text(touches[0].x, 30,30)
-text(touches[0].y, 30,40)
-
-  return false
+return false
 }
 function touchEnded(){
+myConsole()
 
+
+
+}
+
+function myConsole(){
 background(255)
-
+fill(0)
+text("W : " , 10,10)
+text("H : " , 10,20)
+text("X : " , 10,30)
+text("Y : ", 10,40)
+text(windowWidth, 30, 10)
+text(windowHeight, 30, 20)
 text(touches[0].x, 30,30)
 text(touches[0].y, 30,40)
 
